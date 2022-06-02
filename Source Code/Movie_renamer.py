bfd52933f1ff0c0b56a644cfe84df84d736f26f6
Hasitha Suneth <87106402+HasithaSuneth@users.noreply.github.com>
2022-06-03 00:33:47 +0530

    Fix: extension removing issue

--- Source Code/Movie_renamer.py
+++ Source Code/Movie_renamer.py
@@ -1,25 +1,27 @@
-import sys
 import os
-import glob
 import re
+import sys
+import glob
 import string
+from os import system
 from tkinter import filedialog, Tk, messagebox
 
 def get_directory():
     dirname = filedialog.askdirectory(initialdir="/", title='Select Directory')
-    print("Directory : {}".format(dirname))
+    print(" Directory (Preview) : {}".format(dirname))
     response1 = messagebox.askquestion("Question", "Confirm Directory")
     if response1 == "yes":
         return dirname
     else:
         response2 = messagebox.askquestion("Question", "Do you want to try again?")
         if response2 == "yes":
-            get_directory()
+            dirname = get_directory()
+            return dirname
         else:
             sys.exit()
 
 def rename_options(path):
-    usr_input = input("Select an Option : ")
+    usr_input = input(" Select an Option : ")
     if usr_input == '1':
         movie_rename(path)
     elif usr_input == '2':
@@ -36,7 +38,7 @@ def rename_options(path):
             sys.exit()
 
 def clean_rename(path):
-    print("\nUser selection : Clean Name\n")
+    print("\n User selection : Clean Name\n")
     org_names_list, cleaned_list = clean_name(path)
     preview(cleaned_list)
     response4 = messagebox.askquestion("Question", "Do you want to RENAME files/folder as preview?")
@@ -44,7 +46,7 @@ def clean_rename(path):
         for i in range(len(org_names_list)):
             if os.path.exists(path+"\\"+org_names_list[i]):
                 os.rename(path+"\\"+org_names_list[i], path+"\\"+cleaned_list[i])
-        print("\n--- Rename Completed ---")
+        print("\n --- Rename Completed ---")
     else:
         response5 = messagebox.askquestion("Question", "Do you want to start from begining?")
         if response5 == "yes":
@@ -53,7 +55,7 @@ def clean_rename(path):
             sys.exit()
 
 def movie_rename(path):
-    print("\nUser selection : Movie Rename\n")
+    print("\n User selection : Movie Rename\n")
     org_names_list, cleaned_list = clean_name(path)
     movie_rename_list = []
     for i in cleaned_list:
@@ -66,15 +68,14 @@ def movie_rename(path):
                 movie_temp3 = re.search("^([a-zA-Z0-9\'&! ]*) ([0-9]{4})$", i)
                 movie_rename_list.append("{} ({})".format(string.capwords(movie_temp3[1]), movie_temp3[2]))
             except:
-                # print(i)
                 movie_rename_list.append(i)
     preview(movie_rename_list)
     response6 = messagebox.askquestion("Question", "Do you want to RENAME files/folder as preview?")
     if response6 == "yes":
         for i in range(len(org_names_list)):
             if os.path.exists(path+"\\"+org_names_list[i]):
-                os.rename(path+"\\"+org_names_list[i], path+"\\"+movie_rename_list[i])
-        print("\n--- Rename Completed ---")
+                os.rename(path+"\\"+org_names_list[i], path+"\\"+movie_rename_list[i]+os.path.splitext(org_names_list[i])[1])
+        print("\n --- Rename Completed ---")
     else:
         response7 = messagebox.askquestion("Question", "Do you want to start from begining?")
         if response7 == "yes":
@@ -83,14 +84,14 @@ def movie_rename(path):
             sys.exit()
 
 def user_sequence(path):
-    print("\nUser selection : Customize\n")
-    print("Select a Module...\n1. Sub Module\n2. Search Module\n3. Replace Module\n4. Exit")
-    usr_input2 = input("Select an Option : ")
+    print("\n User selection : Customize\n")
+    print(" Select a Module...\n 1. Sub Module\n 2. Search Module\n 3. Replace Module\n 4. Exit")
+    usr_input2 = input(" Select an Option : ")
     if usr_input2 == '1':
-        print("\n--- Sub Module ---\n")
+        print("\n --- Sub Module ---\n")
         sub_list =[]
-        sub_seq_src = input("Enter Sub Sequence SRC: ")
-        sub_seq_dst = input("Enter Sub Sequence DST: ")
+        sub_seq_src = input(" Enter Sub Sequence SRC: ")
+        sub_seq_dst = input(" Enter Sub Sequence DST: ")
         org_names_list = [os.path.basename(t) for t in glob.glob(path + "/*")]
         for i in org_names_list:
             temp = re.sub(sub_seq_src, sub_seq_dst, i)
@@ -101,7 +102,7 @@ def user_sequence(path):
             for i in range(len(org_names_list)):
                 if os.path.exists(path+"\\"+org_names_list[i]):
                     os.rename(path+"\\"+org_names_list[i], path+"\\"+sub_list[i])
-            print("\n--- Rename Completed ---")
+            print("\n --- Rename Completed ---")
         else:
             response10 = messagebox.askquestion("Question", "Do you want to try again?")
             if response10 == "yes":
@@ -110,8 +111,8 @@ def user_sequence(path):
                 sys.exit()
         
     elif usr_input2 == '2':
-        print("\n--- Search Module ---\n")
-        sear_seq = input("Enter Search Sequence: ")
+        print("\n --- Search Module ---\n")
+        sear_seq = input(" Enter Search Sequence: ")
         search_list =[]
         org_names_list = [os.path.basename(t) for t in glob.glob(path + "/*")]
         for i in org_names_list:
@@ -120,14 +121,14 @@ def user_sequence(path):
                 search_list.append(temp[0])
             except:
                 search_list.append(i)
-        print("\nIf File/Folder names show same as before means... Fault Sequence.. Try again\n")
+        print("\n If File/Folder names show same as before means... Fault Sequence.. Try again\n")
         preview(search_list)
         response13 = messagebox.askquestion("Question", "Do you want to RENAME files/folder as preview?")
         if response13 == "yes":
             for i in range(len(org_names_list)):
                 if os.path.exists(path+"\\"+org_names_list[i]):
                     os.rename(path+"\\"+org_names_list[i], path+"\\"+search_list[i])
-            print("\n--- Rename Completed ---")
+            print("\n --- Rename Completed ---")
         else:
             response14 = messagebox.askquestion("Question", "Do you want to try again?")
             if response14 == "yes":
@@ -136,10 +137,10 @@ def user_sequence(path):
                 sys.exit()
 
     elif usr_input2 == '3':
-        print("\n--- Replace Module ---\n")
+        print("\n --- Replace Module ---\n")
         replace_list =[]
-        rep_seq_src = input("Enter Replace Sequence to: ")
-        rep_seq_dst = input("Enter Replace Sequence with: ")
+        rep_seq_src = input(" Enter Replace Sequence to: ")
+        rep_seq_dst = input(" Enter Replace Sequence with: ")
         org_names_list = [os.path.basename(t) for t in glob.glob(path + "/*")]
         for i in org_names_list:
             temp = i.replace(rep_seq_src, rep_seq_dst)
@@ -150,7 +151,7 @@ def user_sequence(path):
             for i in range(len(org_names_list)):
                 if os.path.exists(path+"\\"+org_names_list[i]):
                     os.rename(path+"\\"+org_names_list[i], path+"\\"+replace_list[i])
-            print("\n--- Rename Completed ---")
+            print("\n --- Rename Completed ---")
         else:
             response12 = messagebox.askquestion("Question", "Do you want to try again?")
             if response12 == "yes":
@@ -167,24 +168,26 @@ def user_sequence(path):
             sys.exit()
 
 def preview(in_list):
-    print("Preview....\n")
+    print(" Preview....\n")
     for i in in_list:
-        print(i)
+        print(" "+i)
 
 def clean_name(path):
     clean_name_list = []
     org_names_list = [os.path.basename(t) for t in glob.glob(path + "/*")]
     for i in org_names_list:
-        temp = re.sub("[-.,\(\):_#\[\]]"," ", i).replace("  "," ").replace("  "," ")
+        temp = re.sub("[-.,\(\):_#\[\]$@%=+^]"," ", i).replace("  "," ").replace("  "," ")
         clean_name_list.append(temp)
     return org_names_list, clean_name_list
 
 def main():
     root = Tk().withdraw()
-    print("\n--------- Mass File Renamer ----------\n")
+    system('cls')
+    print("\n\t--------- Mass File Renamer ----------\n")
     direct = get_directory()
+    print("\n Directory (Selected) : {}".format(direct))
 
-    print("\nSelect an option for continue....\n1. Movie Rename\n2. Clean Name\n3. Customize\n4. Exit")
+    print("\n Select an option for continue....\n 1. Movie Rename\n 2. Clean Name\n 3. Customize\n 4. Exit")
     rename_options(direct)
 
 main()
